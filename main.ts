a4309dd7482f2a0fc72f7a36dd09b4006764ba4c
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-11-12 17:21:40 -0500

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
-*/
+// Copyright (c) 2020 MTHS All rights reserved
+// Created by: Arsen Koshparenko
+// Created on: Nov 2024
+// This program sends radio messages to another micro:bit
 
-basic.showString('Hello, World!')
+// Setting the variables
+let distance: number = 0
+
+// Setting the screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Setting the radio group
+radio.setGroup(13)
+
+
+// Main loop for checking the distance
+while (true) {
+    basic.clearScreen()
+    // Setting the distance for sonar in a loop
+    distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    radio.sendNumber(distance)
+}
+
